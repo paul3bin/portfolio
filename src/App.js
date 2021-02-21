3e8657f52965888bccddcd6743ecf1d776752391
Ebin Paul <ebinpaul@outlook.com>
2021-02-21 21:01:09 +0530

    new directory changes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
+import React from 'react';
 import "./App.css";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
-import { faSun } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div>
-      <nav className="navbar fixed-top navbar-expand-lg navbar-light">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Projects
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+
     </div>
   );
 }
