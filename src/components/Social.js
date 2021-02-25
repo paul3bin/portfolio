9be2a97d43140839d377e89ecc4e9c20acc311d8
Ebin Paul <ebinpaul@outlook.com>
2021-02-25 10:27:55 +0530

    added email in social component

--- src/components/Social.js
+++ src/components/Social.js
@@ -10,12 +10,17 @@ class Social extends Component {
         >
           <i class="fab fa-linkedin-in"></i>
         </a>
-        <a href="github.com/paul3bin" target="_blank">
+        <a href="https://github.com/paul3bin" target="_blank">
           <i class="fab fa-github"></i>
         </a>
         <a href="https://twitter.com/paul_ebin" target="_blank">
           <i class="fab fa-twitter"></i>
         </a>
+        <address>
+          <a href="mailto:ebinpaul@outlook.com">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </address>
       </div>
     );
   }
