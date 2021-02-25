f2f70e224be608fc0a22be4b5daea769307dac92
Ebin Paul <ebinpaul@outlook.com>
2021-02-25 10:27:26 +0530

    Resume component

---
+++ src/components/Resume.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import resume from "../files/Resume-Ebin-Paul.pdf";
+
+class Resume extends Component {
+  render() {
+    return (
+      <div className="resume">
+        <a href={resume} target="_blank">
+          <i class="fas fa-file"> Resume</i>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Resume;
