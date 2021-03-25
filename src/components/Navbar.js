22450ecaeaa7cc0b9449b3c237b330ccadefa3dc
Ebin Paul <ebinpaul@outlook.com>
2021-03-25 10:06:12 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,25 +22,70 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav>
-        <ul>
-          <Navitem item="Home" tolink="/" activec={this.activeitem}></Navitem>
-          <Navitem
-            item="About"
-            tolink="/about"
-            activec={this.activeitem}
-          ></Navitem>
-          <Navitem
-            item="Projects"
-            tolink="/Projects"
-            activec={this.activeitem}
-          ></Navitem>
-          <Navitem
-            item="Contact"
-            tolink="/contact"
-            activec={this.activeitem}
-          ></Navitem>
-        </ul>
+      <nav
+        id="navbar"
+        className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark"
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            EP
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-center"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="#home">
+                <Navitem
+                  item="Home"
+                  className="nav-link"
+                  tolink="/"
+                  activec={this.activeitem}
+                ></Navitem>
+              </a>
+              <a className="nav-link active" aria-current="page" href="#about">
+                <Navitem
+                  item="About"
+                  tolink="/about"
+                  activec={this.activeitem}
+                ></Navitem>
+              </a>
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="#projects"
+              >
+                <Navitem
+                  item="Projects"
+                  tolink="/Projects"
+                  activec={this.activeitem}
+                ></Navitem>
+              </a>
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="#contact"
+              >
+                <Navitem
+                  item="Contact"
+                  tolink="/contact"
+                  activec={this.activeitem}
+                ></Navitem>
+              </a>
+            </div>
+          </div>
+        </div>
       </nav>
     );
   }
