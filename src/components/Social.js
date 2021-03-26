3797cfbdc78280ffaa15eb09480e63738e8d6ead
Ebin Paul <ebinpaul@outlook.com>
2021-03-26 12:07:56 +0530

    Update Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,24 +3,30 @@ import React, { Component } from "react";
 class Social extends Component {
   render() {
     return (
-      <div className="social">
-        <a
-          href="https://www.linkedin.com/in/ebin-paul-a89309128/"
-          target="_blank"
-        >
-          <i class="fab fa-linkedin-in"></i>
-        </a>
-        <a href="https://github.com/paul3bin" target="_blank">
-          <i class="fab fa-github"></i>
-        </a>
-        <a href="https://twitter.com/paul_ebin" target="_blank">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <address>
-          <a href="mailto:ebinpaul@outlook.com">
-            <i class="fas fa-envelope"></i>
+      <div className="row social">
+        <div className="col">
+          <a
+            href="https://www.linkedin.com/in/ebin-paul-a89309128/"
+            target="_blank"
+          >
+            <i className="fab fa-linkedin-in"></i>
           </a>
-        </address>
+        </div>
+        <div className="col">
+          <a href="https://github.com/paul3bin" target="_blank">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+        <div className="col">
+          <a href="https://twitter.com/paul_ebin" target="_blank">
+            <i className="fab fa-twitter"></i>
+          </a>
+        </div>
+        <div className="col">
+          <a href="mailto:ebinpaul@outlook.com">
+            <i className="fas fa-envelope"></i>
+          </a>
+        </div>
       </div>
     );
   }
