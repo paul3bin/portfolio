dcd2190fb9a3af14c545c4bc4f7729c6310587a9
Ebin Paul <ebinpaul@outlook.com>
2021-03-27 10:23:59 +0530

    Update Home.js

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -7,16 +7,21 @@ class Home extends Component {
   render() {
     document.title = "Ebin Paul | Home";
     return (
-      <div className="condiv home">
-        <img src={profilePhoto} className="profilepic"></img>
-
-        <ReactTypingEffect
-          className="typingeffect"
-          text={["Hi, I am Ebin Paul", "I love building Web-Apps!"]}
-          speed={100}
-          eraseDelay={700}
-        />
-        <Social />
+      <div className="container">
+        <div className="position-absolute top-0 start-50 translate-middle-x image-pos">
+          <img src={profilePhoto} className="profilepic"></img>
+        </div>
+        <div className="position-absolute top-50 start-50 translate-middle type-pos">
+          <ReactTypingEffect
+            className="typingeffect fs-1"
+            text={["Hi, I am Ebin Paul", "I love building Web-Apps!"]}
+            speed={100}
+            eraseDelay={700}
+          />
+        </div>
+        <div className="position-absolute bottom-0 start-50 translate-middle-x social-pos">
+          <Social />
+        </div>
       </div>
     );
   }
