65724ca4b07c11d79d29dc2b0c837a09d2564bc1
Ebin Paul <ebinpaul@outlook.com>
2021-03-31 09:59:25 +0530

    Update Projects.js

--- src/contents/Projects.js
+++ src/contents/Projects.js
@@ -14,7 +14,7 @@ class Projects extends Component {
               title="SP500 Companies Stock Market Analysis"
               description="A streamlit web-application for stock market analysis of SP500 companies. This project involves web-scraping and data visualization."
               link="https://sp500-stock-market-analysis.herokuapp.com/"
-              link_content="Live Site"
+              github="https://github.com/paul3bin/S-And-P-500-Companies-Stock-Market-Analysis-Web-App"
             />
           </div>
           <div className="col">
@@ -22,7 +22,7 @@ class Projects extends Component {
               title="To-Do Web Application"
               description="A to-do web app using Django, Django REST Framework and ReactJS with user registration and login using token authentication."
               link="https://to-do-application-c9aea.web.app/"
-              link_content="Live Site"
+              github="https://github.com/paul3bin/to_do_frontend"
             />
           </div>
           <div className="col">
@@ -30,7 +30,7 @@ class Projects extends Component {
               title="Crypto Web-App"
               description="A web-application that encrypts and decrypts text using various algorithms like vignere, running key etc."
               link="https://cryptography-io.web.app/"
-              link_content="Live Site"
+              github="https://github.com/paul3bin/cryptography-web-app"
             />
           </div>
           <div className="col">
@@ -38,7 +38,7 @@ class Projects extends Component {
               title="Battle of Neighborhoods"
               description="The scope of this project will be to provide a list of good restaurants based on cuisine, affordability and reviews."
               link="https://github.com/paul3bin/Coursera_Capstone/blob/master/Battle_of_Neighborhoods_Project.ipynb"
-              link_content="Jupyter Notebook"
+              github="https://github.com/paul3bin/Coursera_Capstone"
             />
           </div>
           <div className="col">
@@ -46,7 +46,7 @@ class Projects extends Component {
               title="Segmenting and Clustering Neighborhoods in Toronto"
               description="Using geospatial data of each neighbourhood, getting the top 5 common venues and using K-Means Clustering, seggregating them into clusters."
               link="https://github.com/paul3bin/Coursera_Capstone/blob/master/Segmenting_and_Clustering_Neighborhoods_in_Toronto.ipynb"
-              link_content="Jupyter Notebook"
+              github="https://github.com/paul3bin/Coursera_Capstone"
             />
           </div>
           <div className="col">
@@ -54,7 +54,7 @@ class Projects extends Component {
               title="Kaggle Titanic Dataset Challenge"
               description="This is project based on challenges for the titanic dataset on kaggle.com"
               link="https://github.com/paul3bin/titanic_dataScience/blob/master/Titanic_DataScience_Project.ipynb"
-              link_content="Jupyter Notebook"
+              github="https://github.com/paul3bin/titanic_dataScience"
             />
           </div>
           <div className="col">
@@ -62,7 +62,7 @@ class Projects extends Component {
               title="Connect Four Game"
               description="The objective is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs. The game is built using HTML, CSS and JavaScript."
               link="https://paul3bin.github.io/Connect-Four-Game/"
-              link_content="Live Site"
+              github="https://github.com/paul3bin/Connect-Four-Game"
             />
           </div>
         </div>
